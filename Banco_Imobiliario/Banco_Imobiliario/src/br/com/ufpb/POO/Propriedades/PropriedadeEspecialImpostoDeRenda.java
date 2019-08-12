aaaa69da90a3358d042bbeac11cd458d45676146
AndersonBarbosaDeFreitas <anderson.barbosa@dce.ufpb.br>
2019-08-11 21:51:50 -0300

    Jogo Banco Imobiliário

---
+++ Banco_Imobiliario/Banco_Imobiliario/src/br/com/ufpb/POO/Propriedades/PropriedadeEspecialImpostoDeRenda.java
@@ -0,0 +1,22 @@
+package br.com.ufpb.POO.Propriedades;
+/** Classe para objetos do tipo PropriedadeEspecialImpostoDeRenda, onde serão contidos, valores e métodos para os mesmos.
+ */
+import br.com.ufpb.POO.Jogador;
+
+public class PropriedadeEspecialImpostoDeRenda extends PropriedadeEspecial {
+
+	//Construtor
+	public PropriedadeEspecialImpostoDeRenda(String nome, int indice) {
+		super(nome, indice);
+	}
+	
+	/** Método para que o jogador pague o imposto de renda
+	 *  @param jogador Jogador - Jogador que pagará o imposto
+	 */
+	public void funcaoTabuleiro(Jogador jogador) {
+		jogador.pagarTaxa(200);
+		System.out.println(""+jogador.getNome()+" paga $200.");
+		System.out.println("Seu novo saldo é: $"+jogador.getSaldo());
+	}
+
+}
