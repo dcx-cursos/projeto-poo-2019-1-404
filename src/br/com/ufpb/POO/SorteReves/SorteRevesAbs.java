e2484d1c6e1795e5b858a9419185fe7201d717b7
anderson.barbosa <anderson.barbosa@LAB3-DCX-37-LNX>
2019-08-30 16:13:19 -0300

    Classe carta abstrata Sorte Revés

---
+++ src/br/com/ufpb/POO/SorteReves/SorteRevesAbs.java
@@ -0,0 +1,24 @@
+package br.com.ufpb.POO.SorteReves;
+
+import br.com.ufpb.POO.Jogador;
+import br.com.ufpb.POO.Listas;
+
+public abstract class SorteRevesAbs {
+	
+	private int numberCard;
+
+	public String funcaoCarta(Jogador jogadorDaVez, Listas lista)
+	{
+		return "";
+	}
+	
+	public int getNumber()
+	{
+		return this.numberCard;
+	}
+	
+	public void setNumber(int number)
+	{
+		this.numberCard = number;
+	}
+}
