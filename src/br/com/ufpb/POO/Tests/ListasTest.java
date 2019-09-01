6ca49ba8021baf792f35fa478d4438ae00a8c7ec
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-09-01 00:02:52 -0300

    Ajuste no Before

--- src/br/com/ufpb/POO/Tests/ListasTest.java
+++ src/br/com/ufpb/POO/Tests/ListasTest.java
@@ -2,6 +2,7 @@ package br.com.ufpb.POO.Tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.ufpb.POO.Jogador;
@@ -13,17 +14,29 @@ import br.com.ufpb.POO.Propriedades.PropriedadeComercializavel;
 import br.com.ufpb.POO.Propriedades.PropriedadeComercializavelImovel;
 
 public class ListasTest {
-
+	public Listas lista;
+	public Tabuleiro tabuleiro;
+	public Jogador jogador1;
+	public Jogador jogador2;
+	public Jogador jogador3;
+	public Jogador jogador4;
+	@Before
+	public void inciaObjetos() {
+		lista = new Listas();
+		tabuleiro = new Tabuleiro(lista);
+		jogador1 = new Jogador("Ismar", "Azul",lista.getTabuleiro().get(0),1500);
+		jogador1.setId(0);
+		jogador2 = new Jogador("Anderson", "Preto", lista.getTabuleiro().get(6), 1500);
+		jogador2.setId(1);
+		jogador3 = new Jogador("Jobson", "Verde", lista.getTabuleiro().get(1), 1500);
+		jogador4 = new Jogador("Josué", "Vermelho", lista.getTabuleiro().get(2), 1500);
+		
+	}
 	@Test
 	public void testAddJogador() {
-		Listas lista = new Listas();
-		Tabuleiro tabuleiro = new Tabuleiro(lista);
-		CasaDoTabuleiro casa = new Ponto_de_Partida("Ponto de Partida", 0);
-		Jogador jogador = new Jogador("Ismar", "Azul", casa, 1500);
-		lista.addJogador(jogador);
-		assertEquals(lista.getJogadores().get(0),jogador);
+		lista.addJogador(jogador1);
+		assertEquals(lista.getJogadores().get(0),jogador1);
 		assertEquals(1,lista.getJogadores().size());
-		Jogador jogador2 = new Jogador("Anderson", "Preto", lista.getTabuleiro().get(6), 1500);
 		lista.addJogador(jogador2);
 		assertEquals(lista.getJogadores().get(1),jogador2);
 		assertEquals(2,lista.getJogadores().size());
@@ -31,12 +44,6 @@ public class ListasTest {
 
 	@Test
 	public void testAddListaDeJogadoresNaCasaDePartida(){
-		Listas lista = new Listas();
-		Tabuleiro tabuleiro = new Tabuleiro(lista);
-		Jogador jogador1 = new Jogador("Ismar", "Azul",lista.getTabuleiro().get(0),1500);
-		Jogador jogador2 = new Jogador("Anderson", "Preto", lista.getTabuleiro().get(6), 1500);
-		Jogador jogador3 = new Jogador("Jobson", "Verde", lista.getTabuleiro().get(1), 1500);
-		Jogador jogador4 = new Jogador("JosuÃ©", "Vermelho", lista.getTabuleiro().get(2), 1500);
 		lista.addJogador(jogador1);
 		lista.addJogador(jogador2);
 		lista.addJogador(jogador4);
@@ -58,10 +65,6 @@ public class ListasTest {
 	
 	@Test
 	public void testGetJogadorId(){
-		Listas lista = new Listas();
-		Tabuleiro tabuleiro = new Tabuleiro(lista);
-		Jogador jogador1 = new Jogador("Ismar", "Azul",lista.getTabuleiro().get(0),1500);
-		Jogador jogador2 = new Jogador("Anderson", "Preto", lista.getTabuleiro().get(6), 1500);
 		lista.addJogador(jogador1);
 		lista.addJogador(jogador2);
 		lista.addListaDeJogadoresNaCasaDePartida();
