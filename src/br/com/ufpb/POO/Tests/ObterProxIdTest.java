01b6a6d85fae3e189778591ef0b341681b3987bd
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-08-31 23:55:34 -0300

    Ajuste do Before

--- src/br/com/ufpb/POO/Tests/ObterProxIdTest.java
+++ src/br/com/ufpb/POO/Tests/ObterProxIdTest.java
@@ -2,6 +2,7 @@ package br.com.ufpb.POO.Tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.ufpb.POO.Jogador;
@@ -11,39 +12,48 @@ import br.com.ufpb.POO.Tabuleiro;
 
 
 public class ObterProxIdTest {
+	public ObterProxId obterProxId;
+	public Listas lista;
+	public Tabuleiro tabuleiro;
+	public Jogador jogador1;
+	public Jogador jogador2;
+	public Jogador jogador3;
 	
-	ObterProxId obterProxId = new ObterProxId();
-	Listas lista = new Listas();
-	Tabuleiro tabuleiro = new Tabuleiro(lista);
-	Jogador jogador = new Jogador("Anderson", "Preto",lista.getTabuleiro().get(0),1500);
-	Jogador jogador2 = new Jogador("Ismar", "Azul", lista.getTabuleiro().get(6), 1500);
-	Jogador jogador3 = new Jogador("JosuÃ©", "Vermelho", lista.getTabuleiro().get(1), 1500);
-	
+	@Before
+	public void iniciaObjetos(){
+		obterProxId = new ObterProxId();
+		lista = new Listas();
+		tabuleiro = new Tabuleiro(lista);
+		jogador1 = new Jogador("Anderson", "Preto",lista.getTabuleiro().get(0),1500);
+		jogador2 = new Jogador("Ismar", "Azul", lista.getTabuleiro().get(6), 1500);
+		jogador3 = new Jogador("Josué", "Vermelho", lista.getTabuleiro().get(1), 1500);
+		
+	}
 	@Test
 	public void testObterIdProxCasa() {
-		lista.addJogador(jogador);
+		lista.addJogador(jogador1);
 		lista.addListaDeJogadoresNaCasaDePartida();
-		jogador.getPos().mover(jogador, lista.getTabuleiro().get(6));
+		jogador1.getPos().mover(jogador1, lista.getTabuleiro().get(6));
 		//Passo 1
-		int proxId = obterProxId.obterIdProxCasa(jogador.getPos().getIndice(),7,jogador);
+		int proxId = obterProxId.obterIdProxCasa(jogador1.getPos().getIndice(),7,jogador1);
 		assertEquals(13,proxId);
-		jogador.getPos().mover(jogador, lista.getTabuleiro().get(13));
-		int proxId2 = obterProxId.obterIdProxCasa(jogador.getPos().getIndice(),12,jogador);
+		jogador1.getPos().mover(jogador1, lista.getTabuleiro().get(13));
+		int proxId2 = obterProxId.obterIdProxCasa(jogador1.getPos().getIndice(),12,jogador1);
 		assertEquals(25,proxId2);
-		jogador.getPos().mover(jogador, lista.getTabuleiro().get(25));
-		int proxId3 = obterProxId.obterIdProxCasa(jogador.getPos().getIndice(),5,jogador);
+		jogador1.getPos().mover(jogador1, lista.getTabuleiro().get(25));
+		int proxId3 = obterProxId.obterIdProxCasa(jogador1.getPos().getIndice(),5,jogador1);
 		assertEquals(30,proxId3);
-		jogador.getPos().mover(jogador, lista.getTabuleiro().get(30));
-		int proxId4 = obterProxId.obterIdProxCasa(jogador.getPos().getIndice(),10,jogador);
+		jogador1.getPos().mover(jogador1, lista.getTabuleiro().get(30));
+		int proxId4 = obterProxId.obterIdProxCasa(jogador1.getPos().getIndice(),10,jogador1);
 		assertEquals(0,proxId4);	
 	}
 	@Test
 	public void testObterProxJogador() {
-		lista.addJogadorJogando(jogador);
+		lista.addJogadorJogando(jogador1);
 		lista.addJogadorJogando(jogador2);
 		lista.addJogadorJogando(jogador3);
 		//Passo 1
-		assertEquals(1,obterProxId.obterIdProxJogador(jogador.getId(), lista.getJogadoresJogando()));
+		assertEquals(1,obterProxId.obterIdProxJogador(jogador1.getId(), lista.getJogadoresJogando()));
 		assertEquals(2,obterProxId.obterIdProxJogador(jogador2.getId(), lista.getJogadoresJogando()));
 		assertEquals(0,obterProxId.obterIdProxJogador(jogador3.getId(), lista.getJogadoresJogando()));
 		
