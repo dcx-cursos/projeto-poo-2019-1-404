c8eb668438cdab16b41365e2b58fe18970dfc412
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-09-22 19:59:17 -0300

    Refatoração da Classe PropriedadeEspecialSorteReves

--- src/br/com/ufpb/POO/Propriedades/PropriedadeEspecialSorteReves.java
+++ src/br/com/ufpb/POO/Propriedades/PropriedadeEspecialSorteReves.java
@@ -4,37 +4,22 @@ package br.com.ufpb.POO.Propriedades;
 
 import br.com.ufpb.POO.Jogador;
 import br.com.ufpb.POO.Listas;
+import br.com.ufpb.POO.SorteReves.SorteReves;
 
 public class PropriedadeEspecialSorteReves extends PropriedadeEspecial {
-	private Listas lista;
 	
 	//Construtor
-	public PropriedadeEspecialSorteReves(String nome, int indice, Listas lista) {
+	public PropriedadeEspecialSorteReves(String nome, int indice) {
 		super(nome, indice);
-		this.lista = lista;
 	}
 	
-	/** AINDA SERÃ� IMPLEMENTADO
+	/** Realiza a função referente a carta recebida pelo jogador
 	 * @param jogador Jogador 
 	 */
 	public void funcaoTabuleiro(Jogador jogador) {
-//		System.out.print("Sorte ou RevÃ©s, "+jogador.getNome()+" nada acontece!\n");
-		if(this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).getNumber() == 2)
-		{
-			System.out.println(this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).funcaoCarta(lista));
-			this.lista.getDeck().desinfileirar();
-		}else if(this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).getNumber() == 6
-				|| this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).getNumber() == 7) {
-			System.out.println(this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).funcaoCarta(jogador,lista));
-			this.lista.getDeck().desinfileirar();
-		}
-		
-		else
-		{
-			System.out.println(this.lista.getDeck().getLista().get(this.lista.getDeck().getLista().size() - 1).funcaoCarta(jogador));
-			this.lista.getDeck().desinfileirar();			
-		}
-
+		int cartaRetirada = Listas.getInstance().getDeck().remove();
+		SorteReves.pagarOuReceberValorCarta(cartaRetirada, jogador);
+		Listas.getInstance().getDeck().desinfileirar(cartaRetirada);
 	}
 	
 }
