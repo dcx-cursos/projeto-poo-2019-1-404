dbdbeb509496a98ca3f27f10c8422b7c20eb5435
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-09-22 19:52:13 -0300

    Atualização da classe PropriedadeComercializavelImovel

--- src/br/com/ufpb/POO/Propriedades/PropriedadeComercializavelImovel.java
+++ src/br/com/ufpb/POO/Propriedades/PropriedadeComercializavelImovel.java
@@ -1,5 +1,5 @@
 package br.com.ufpb.POO.Propriedades;
-/** Classe para objetos do tipo PropriedadeComercializavelImovel, onde serão contidos, valores e métodos para os mesmos.
+/** Classe para objetos do tipo PropriedadeComercializavelImovel, onde serão contidos, valores e m�todos para os mesmos.
  */
 
 import java.util.Scanner;
@@ -9,6 +9,7 @@ import br.com.ufpb.POO.Verificadores.VerificadoresDojogo;
 
 public class PropriedadeComercializavelImovel extends PropriedadeComercializavel {
 	
+	private int contCasas = 0;
 	private Scanner scan;
 	private VerificadoresDojogo verificador = new VerificadoresDojogo();
 	//Construtor
@@ -18,14 +19,23 @@ public class PropriedadeComercializavelImovel extends PropriedadeComercializavel
 		this.scan = new Scanner(System.in);
 	}
 	
-	/** Método para comprar a propriedade verificando se ela já está vendida
+	public int getContCasas() {
+		return contCasas;
+	}
+	
+	public void setContCasas(int number)
+	{
+		this.contCasas += number;
+	}
+	
+	/** M�todo para comprar a propriedade verificando se ela já está vendida
 	 *  @param jogador Jogador - Comprador
 	 */
 	public void funcaoTabuleiro(Jogador jogador) {
 		if(!this.estaVendido()) {
-			System.out.println("O título da propriedade "+ this.getNome()+" está disponível por $"+this.getValor()+".");
+			System.out.println("O t�tulo da propriedade "+ this.getNome()+" est� disponível por $"+this.getValor()+".");
 			System.out.println(""+jogador.getNome()+" você possui $"+jogador.getSaldo()+".");
-			System.out.print("Você deseja comprar "+this.getNome()+" (Sim/Não)? ");
+			System.out.print("Você deseja comprar "+this.getNome()+" (Sim/N�o)? ");
 			String resposta = this.scan.nextLine().toUpperCase();
 			if(resposta.equals("S") || resposta.equals("SIM")) {
 				if(verificador.podeComprar(jogador, this)) {
@@ -39,7 +49,7 @@ public class PropriedadeComercializavelImovel extends PropriedadeComercializavel
 		
 		else {
 			if(!this.getProprietario().equals(jogador)) {
-				System.out.println("A propriedade já está vendida, "+jogador.getNome()+" paga $"+this.getTaxa()+".");
+				System.out.println("A propriedade já est� vendida, "+jogador.getNome()+" paga $"+this.getTaxa()+".");
 				jogador.pagarTaxa(this.getTaxa());
 				this.getProprietario().receberTaxa(this.getTaxa());
 				System.out.println(""+this.getProprietario().getNome()+" recebe $"+this.getTaxa());
@@ -49,4 +59,5 @@ public class PropriedadeComercializavelImovel extends PropriedadeComercializavel
 			}
 		}
 	}
+
 }
