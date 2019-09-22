f30298672fc47d9d0fcd20208c0ecadd49b96ef8
josueAMN <josue.alves@dcx.ufpb.br>
2019-09-22 18:25:02 -0300

    Atualizacao da classe ControlesTabuleiro

--- src/br/com/ufpb/POO/ControlesTabuleiro.java
+++ src/br/com/ufpb/POO/ControlesTabuleiro.java
@@ -1,98 +1,103 @@
 package br.com.ufpb.POO;
 
+import java.util.ArrayList;
+
 /**Classe para objetos do tipo ControlesTabuleiro, onde serão contido, valores e métodos do mesmo.
  * @author Anderson, Ismar, Jobson, Josué
  */
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Random;
 
 import br.com.ufpb.POO.Propriedades.CasaDoTabuleiro;
 import br.com.ufpb.POO.Verificadores.VerificadoresDojogo;
 
 public class ControlesTabuleiro {
-	private Tabuleiro tabuleiro;
-	private Jogador jogadorDaVez;
-	private boolean executando = true;
-	private Listas listas;
+	
+	private ArrayList<String> comandosDisponiveis;
+	private static Jogador jogadorDaVez;
 	private InsereJogadores insereJogadores;
-	private ObterProxId obterProxId;
 	private Scanner scan;
-	public Dados dados;
+	public static Dados dados;
 	private VerificadoresDojogo verificador = new VerificadoresDojogo();
-	private int qtdJogadasSeguidas =  0;
-	private boolean dadosIguais = false;
+	private static int qtdJogadasSeguidas =  0;
+	private static boolean dadosIguais = false;
 			
 	public ControlesTabuleiro() {
-		this.listas = new Listas();
-		this.tabuleiro = new Tabuleiro(this.listas);
-		this.obterProxId = new ObterProxId();
-		this.insereJogadores = new InsereJogadores(this.listas);
+		this.comandosDisponiveis = new ArrayList<String>();
+    	Tabuleiro.getInstance();
+		this.insereJogadores = new InsereJogadores();
 		this.scan = new Scanner(System.in);
-		this.dados = new Dados();
+		ControlesTabuleiro.dados = new Dados();
+		this.inicializaComandos();
 	}
+	
+	public Jogador getJogadorDaVez() {
+		return jogadorDaVez;
+	}
+	
+	public void setJogadorDaVez() {
+		jogadorDaVez = Listas.getInstance().getJogador();
+	}
+	
 	/**Método para inserir jogadores, onde realiza a chamada do método inciaJogadorDaVez*/
 	
-	public void inserirJogadores() {
-		this.insereJogadores.inserirJogador();
-		iniciaJogadorDaVez();
+	public void inserirJogadores(String nome, String corPeao) {
+		this.insereJogadores.inserirJogador(nome, corPeao);
 	}
 
 	/**Método para iniciar o Jogador da vez, onde realiza a chamada do método options*/
 	
 	public void iniciaJogadorDaVez() {
-		this.jogadorDaVez = this.listas.getPrimeiroJogador();
-		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-		System.out.println("O Banco Imobiliário vai começar. Aproveite!");
-		options(this.jogadorDaVez);
+		ControlesTabuleiro.jogadorDaVez = Listas.getInstance().getJogador();
 	}
 
 	/**Método para mover o Jogador pelo tabuleiro
 	 * @param Jogador - Jogador da vez
 	 * @param CasaDoTabuleiro - destino do Jogador*/
 	
-	public void moverJogador(Jogador jogadorDaVez, CasaDoTabuleiro destino) {
-		destino.mover(this.jogadorDaVez, destino);
+	public static void moverJogador(Jogador jogadorDaVez, CasaDoTabuleiro destino) {
+		destino.mover(jogadorDaVez, destino);
 	}
-
-	/**Método que altera o Jogador da vez
-	 * @param int - id do Jogador da vez
-	 * @param ArrayList<Jogador> - lista de jogadores*/
 	
-	public void mudaJogadorDaVez(int idJogador, ArrayList<Jogador> jogadores) {
-		int id = this.obterProxId.obterIdProxJogador(idJogador, jogadores);
-		this.jogadorDaVez = this.listas.getJogadorId(id);
-//		System.out.println(this.jogadorDaVez = this.listas.getJogadorId(id));
-//		if(listas.getJogadoresPresos().contains(this.jogadorDaVez)) {
-//			System.out.print("Jogador"+this.jogadorDaVez.getNome()+" está preso!");
-//			int novoId = this.jogadorDaVez.getId();
-//			this.jogadorDaVez = this.listas.getJogadorId(this.obterProxId.obterIdProxJogador(novoId, jogadores));
-//		}
+	/**Método para obter o Id da próxima casa, a qual o jogador irá chegar
+	 * @param int - Id atual do Jogador
+	 * @param int - resultado da soma dos valores dos dados
+	 * @param Jogador - Jogador da vez
+	 * @return int - Id da próxima casa*/
+
+	public static int obterIdProxCasa(int idAtual, int resultado, Jogador jogadorDaVez) {
+		int idProxCasa, temp = 0;
+		idProxCasa = idAtual + resultado;
+		if(idProxCasa > 39) {
+			Listas.getInstance().getTabuleiro().get(0).funcaoTabuleiro(jogadorDaVez);
+			temp = idProxCasa % 40;
+			idProxCasa = temp;
+		}
+		return idProxCasa;
 	}
 
 	/**Método para realizar o "lançamento" dos dados*/
 	
-	public void jogarDados() {
+	public static void jogarDados() {
 		dados.lancarDados();
-		CasaDoTabuleiro casaJogador = this.jogadorDaVez.getPos();
+		CasaDoTabuleiro casaJogador = jogadorDaVez.getPos();
 
 		//Verificar se o jogador jogou mais tr�s vezes seguidas e prend�-lo.
 
-		if (dados.dado1==dados.dado2) {
-			this.qtdJogadasSeguidas+=1;
-			System.out.println("\nDados iguais\n"+this.qtdJogadasSeguidas);
+		if (Dados.getDado1()==Dados.getDado2()) {
+			qtdJogadasSeguidas+=1;
+			System.out.println("\nDados iguais\n"+qtdJogadasSeguidas);
 			moverJogador(jogadorDaVez, casaJogador);
-			this.dadosIguais = true;
+			dadosIguais = true;
 		}
 		
-		int indiceCasaDestino = this.obterProxId.obterIdProxCasa(this.jogadorDaVez.getIdAtualDoJogador(), dados.resultado(), this.jogadorDaVez);
-		System.out.println("O jogador "+this.jogadorDaVez.getNome()+" ("+this.jogadorDaVez.getCorPeao()+") tirou "+dados.getDado1()+","+dados.getDado2()+
-		" o peão avançou para "+indiceCasaDestino+" - "+this.listas.getTabuleiro().get(indiceCasaDestino).getNome());
-		moverJogador(jogadorDaVez, this.listas.getCasaById(indiceCasaDestino));
-		this.listas.getCasaById(indiceCasaDestino).funcaoTabuleiro(jogadorDaVez);
+		int indiceCasaDestino = obterIdProxCasa(jogadorDaVez.getIdAtualDoJogador(), dados.resultado(), jogadorDaVez);
+		System.out.println("O jogador "+jogadorDaVez.getNome()+" ("+jogadorDaVez.getCorPeao()+") tirou "+Dados.getDado1()+","+Dados.getDado2()+
+		" o peão avançou para "+indiceCasaDestino+" - "+Listas.getInstance().getTabuleiro().get(indiceCasaDestino).getNome());
+		moverJogador(jogadorDaVez, Listas.getInstance().getCasaById(indiceCasaDestino));
+		Listas.getInstance().getCasaById(indiceCasaDestino).funcaoTabuleiro(jogadorDaVez);
 		
-		this.dadosIguais = false;
+		dadosIguais = false;
 	}
 	
 	public void jogardadosCarta() {
@@ -102,10 +107,10 @@ public class ControlesTabuleiro {
 	/**Método para verificar se o Jogador realmente deseja sair*/
 	
 	public boolean optionSair(){
-		System.out.println("Deseja realmente sair?[s/n]");
+		System.out.print("Deseja realmente sair?[s/n] ");
 		String escolha2 = this.scan.nextLine().toUpperCase();
 		if(escolha2.equals("S")) {
-			this.listas.setJogadoresJogando(jogadorDaVez);
+			Listas.getInstance().setJogadorSaiu(ControlesTabuleiro.jogadorDaVez);
 			return true;
 		} else if(!escolha2.equals("N")) {
 			System.out.println("Comando indisponível!");
@@ -114,49 +119,113 @@ public class ControlesTabuleiro {
 		return false;
 	}
 
-	/**Método que mostra as opções(operações) do jogo para o jogador, onde realiza a chamada de todos os métodos que sejam necessarios para realizar essas operações.
-	 * @param Jogador - Jogador da vez*/
+	public void inicializaComandos() {
+		this.comandosDisponiveis.add(0, "[sair]");
+		this.comandosDisponiveis.add(0, "[status]");
+		this.comandosDisponiveis.add(0, "[jogar]");
+	}
 	
-	public void options(Jogador jogadorDaVez) {
-		if(this.listas.getJogadoresJogando().size()>1){
-			System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-			System.out.print("A jogada de "+jogadorDaVez.getNome()+" ("+jogadorDaVez.getCorPeao()+") começou.\n");
-			System.out.println("Comandos disponíveis: [jogar][status][sair]");
-			System.out.print("Entre com um comando: ");
-			String escolha = this.scan.nextLine().toUpperCase();
-			if(escolha.equals("S") || escolha.equals("SAIR")) {
-				if(!optionSair()) {
-					options(jogadorDaVez);
-				}
-			} if (escolha.equals("J") || escolha.equals("JOGAR")) {
-				if(this.dadosIguais)
-				{
-					jogarDados();
-				}
-				if(this.dadosIguais && this.qtdJogadasSeguidas>=3) {
-					this.jogadorDaVez.setEstaPreso(true);
-					this.qtdJogadasSeguidas = 0;
-					this.listas.getCasaById(30).funcaoTabuleiro(this.jogadorDaVez);
-				}
-				this.qtdJogadasSeguidas = 0;
-				jogarDados();
-			} if(escolha.equals("STATUS")){
-				System.out.println(jogadorDaVez.toStatus());
-				options(jogadorDaVez);
-			}
-			mudaJogadorDaVez(jogadorDaVez.getId(), this.listas.getJogadoresJogando());
-			options(this.jogadorDaVez);
-		} else {
-			this.executando = false;
+	public void adicionaComandoVender() {
+		if(!this.comandosDisponiveis.contains("[vender]"))
+		{
+			this.comandosDisponiveis.add(0,"[vender]");
 		}
+	}
+	
+	public void adicionaComandoConstruir() {
+		if(!this.comandosDisponiveis.contains("[construir]"))
+		{
+			this.comandosDisponiveis.add(0,"[construir]");			
+		}
+	}
+	
+	public void rmComandoConstruir() {
+		if(this.comandosDisponiveis.contains("[construir]"))
+		{
+			this.comandosDisponiveis.remove("[construir]");			
+		}
+	}
+	
+	public void rmComandoVender() {
+		if(this.comandosDisponiveis.contains("[vender]"))
+		{
+			this.comandosDisponiveis.remove("[vender]");
+		}
+	}
+	
+	public String getComandos() {
+		String comandos = "";
+		for(String s: this.comandosDisponiveis) {
+			comandos += ""+s;
+		}
+		return comandos;
+	}
+	
+	public void Construir() {
+		jogadorDaVez.setIdPropriedadesParaConstruir();
+		while(this.verificador.verificadorPossuiSaldoParaConstruir(jogadorDaVez))
+		{
+		System.out.println(""+jogadorDaVez.getNome()+" possui $"+jogadorDaVez.getSaldo());
+		System.out.println(jogadorDaVez.toPropriedadesParaConstruir());
+			System.out.print("Digite o número da propriedade (0 para sair): ");
+			int escolhaConstruir = Integer.parseInt(this.scan.nextLine());
+			if(escolhaConstruir >= 1)
+			{
+				if(this.verificador.verificadorJaTemHotel(jogadorDaVez, escolhaConstruir)) {
+					System.out.println("Não pode construir, já existe um hotel construído.");
+				}
+				else {
+					jogadorDaVez.pagarTaxa(jogadorDaVez.getPropriedadesConstruir().get(escolhaConstruir - 1).getValorCasa());
+					jogadorDaVez.getPropriedadesConstruir().get(escolhaConstruir - 1).setContCasas(1);					
+				}
+			}
+			if(escolhaConstruir == 0) {
+				break;
+			}
+		}
+	}
+	
+	public void Vender() {
+		jogadorDaVez.setIdPropriedadesParaConstruir();
+		while(this.verificador.verificadorPossuiSaldoParaConstruir(jogadorDaVez))
+		{
+		System.out.println(""+jogadorDaVez.getNome()+" possui $"+jogadorDaVez.getSaldo());
+		System.out.println(jogadorDaVez.toPropriedadesParaConstruir());
+			System.out.print("Digite o número da propriedade (0 para sair): ");
+			int escolhaVender = Integer.parseInt(this.scan.nextLine());
+			if(escolhaVender >= 1)
+			{
+				if(this.verificador.verificadorTemCasaParaVender(jogadorDaVez, escolhaVender)) {
+					jogadorDaVez.receberTaxa(jogadorDaVez.getPropriedadesConstruir().get(escolhaVender - 1).getValorCasa());
+					jogadorDaVez.getPropriedadesConstruir().get(escolhaVender - 1).setContCasas(-1);					
+				}
+				else {
+					break;
+				}
+			}
+			if(escolhaVender == 0) {
+				break;
+			}
+		}
+	}
+	
+	public void Jogar() {
+		if(ControlesTabuleiro.dadosIguais)
+		{
+			jogarDados();
+		}
+		if(ControlesTabuleiro.dadosIguais && ControlesTabuleiro.qtdJogadasSeguidas>=3) {
+			ControlesTabuleiro.jogadorDaVez.setEstaPreso(true);
+			ControlesTabuleiro.qtdJogadasSeguidas = 0;
+			Listas.getInstance().getCasaById(30).funcaoTabuleiro(ControlesTabuleiro.jogadorDaVez);
+		}
+		ControlesTabuleiro.qtdJogadasSeguidas = 0;
+		jogarDados();
+	}
+	
+	public String getVencedor()
+	{
+		return "\nJogador "+Listas.getInstance().getJogadoresJogando().get(0).getNome()+" - "+Listas.getInstance().getJogadoresJogando().get(0).getCorPeao()+", é o Vencedor!";
 	}
 
-	/**Método que inicia o Jogo, onde realiza a chamada do método do inserirJogadores*/
-	
-	public void iniciarJogo() {
-		if(this.executando) {
-			inserirJogadores();
-		}
-		System.out.println("Jogador "+this.listas.getJogadoresJogando().get(0).getNome()+" - "+this.listas.getJogadoresJogando().get(0).getCorPeao()+", é o Vencedor!");			
-	}
 }
